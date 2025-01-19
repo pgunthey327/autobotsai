b16843e288c7f5faa46509a01e34344a33bbffad
pgunthey327 <meshprath@gmail.com>
2025-01-19 22:29:49 +0530

    ui fix

--- src/App.jsx
+++ src/App.jsx
@@ -479,9 +479,9 @@ const runGemini2Model = async prompt => {
                       placeholder="Enter Message"
                       aria-describedby="basic-addon2"
                     />
-                    <div className="tooltip">
+                    <div className="tooltip" style={{ pointerEvents:(loader1 || loader2 || !toggleBot)?"none":"all"}}>
                         <span className="tooltiptext">Send</span>
-                        <InputGroup.Text className="button-click" id="basic-addon2" style={{pointerEvents:(loader1 || loader2 || !toggleBot)?"none":"all", height: "100%"}} onClick={()=>{
+                        <InputGroup.Text className="button-click" id="basic-addon2" style={{height: "100%"}} onClick={()=>{
                           if(geminiFlash){
                             handleUser1Message(userMessage);
                           }
@@ -490,7 +490,7 @@ const runGemini2Model = async prompt => {
                           }
                           setImageSelected(null);
                           }}>
-                          <svg  style={{width:"46px", height:"25px", pointerEvents:(loader1 || loader2 || !toggleBot)?"none":"all"}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#B197FC" d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1S160 493.2 160 480l0-83.6c0-4 1.5-7.8 4.2-10.8L331.8 202.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8 17.7 316.6C7.1 311.3 .3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z"/></svg>
+                          <svg  style={{width:"46px", height:"25px"}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#B197FC" d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1S160 493.2 160 480l0-83.6c0-4 1.5-7.8 4.2-10.8L331.8 202.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8 17.7 316.6C7.1 311.3 .3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z"/></svg>
                         </InputGroup.Text>
                       </div>
               </InputGroup>
