a9b19677815ae0f781662acbfa1035225b21a74f
pgunthey327 <meshprath@gmail.com>
2025-01-19 23:28:23 +0530

    UI fix 3

--- src/App.jsx
+++ src/App.jsx
@@ -383,7 +383,7 @@ const runGemini2Model = async prompt => {
             </Row>
          
             <Row style={{height:"84%", padding:"10px", display:toggleBot?"flex":"none"}}>
-              {(model==="All AI Modal" || model==="Gemini 1.5 Flash") && (<Col style={{display: "flex", flexDirection: "row", justifyContent: "center"}}>
+              {(model==="All AI Modal" || model==="Gemini 1.5 Flash") && (<Col style={{display: "flex", flexDirection: "row", justifyContent: "center", width:"600px"}}>
                 <div style={{width:"100%", height:"100%", padding:"10px", backgroundColor:"#b892c6", maxWidth: "720px"}}>
                 <div style={{color: "black", fontSize: "x-large", textAlign: "center", marginBottom: "5px", borderBottom:"2px solid grey", display: "inline-flex", width: "100%", justifyContent: "space-between"}}>
                 <Form.Check
@@ -398,7 +398,7 @@ const runGemini2Model = async prompt => {
                   
                 </div>
               </Col>)}
-              {(model==="All AI Modal" || model==="Gemini 1.5 Pro") && (<Col style={{display: "flex", flexDirection: "row", justifyContent: "center"}}>
+              {(model==="All AI Modal" || model==="Gemini 1.5 Pro") && (<Col style={{display: "flex", flexDirection: "row", justifyContent: "center", width:"600px"}}>
               <div style={{width:"100%", height:"100%", padding:"10px", backgroundColor:"#b892c6", maxWidth: "720px"}}>
                 <div style={{color: "black", fontSize: "x-large", textAlign: "center", marginBottom: "5px", borderBottom:"2px solid grey", display: "inline-flex", width: "100%", justifyContent: "space-between"}}>   
                 <Form.Check
@@ -416,13 +416,13 @@ const runGemini2Model = async prompt => {
             </Row>
             <Row style={{height:"50px", width: "95%", display: "flex", justifySelf: "center"}}>
               <InputGroup className="mb-3" style={{marginBottom:"0rem !important"}}>
-                  <InputGroup.Text className="button-click" onClick={() => setToggleBot(!toggleBot)} id="basic-addon2">
-                    <div className="tooltip" style={{width:"100%"}}>
-                      <span className="tooltiptext">{toggleBot? "Generate Template": "Generate Code"}</span>
+              <div className="tooltip" style={{height:"100%"}}>
+              <span className="tooltiptext">{toggleBot? "Generate Template": "Generate Code"}</span>
+                  <InputGroup.Text style={{height:"100%"}} className="button-click" onClick={() => setToggleBot(!toggleBot)} id="basic-addon2">
                       {toggleBot && (<svg style={{width:"20px"}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="#B197FC" d="M0 64C0 28.7 28.7 0 64 0L224 0l0 128c0 17.7 14.3 32 32 32l128 0 0 288c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 64zm384 64l-128 0L256 0 384 128z"/></svg>)}
                       {!toggleBot && (<svg style={{width:"20px"}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#B197FC" d="M160 368c26.5 0 48 21.5 48 48l0 16 72.5-54.4c8.3-6.2 18.4-9.6 28.8-9.6L448 368c8.8 0 16-7.2 16-16l0-288c0-8.8-7.2-16-16-16L64 48c-8.8 0-16 7.2-16 16l0 288c0 8.8 7.2 16 16 16l96 0zm48 124l-.2 .2-5.1 3.8-17.1 12.8c-4.8 3.6-11.3 4.2-16.8 1.5s-8.8-8.2-8.8-14.3l0-21.3 0-6.4 0-.3 0-4 0-48-48 0-48 0c-35.3 0-64-28.7-64-64L0 64C0 28.7 28.7 0 64 0L448 0c35.3 0 64 28.7 64 64l0 288c0 35.3-28.7 64-64 64l-138.7 0L208 492z"/></svg>)}
-                    </div>
                   </InputGroup.Text>
+                  </div>
                   <div>
                     <div style={{display:"flex", flexDirection:"row"}}>
                     <InputGroup.Text id="basic-addon2" className="button-click" style={{pointerEvents:(loader1 || loader2 || !toggleBot)?"none":"all"}}>
